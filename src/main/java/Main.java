6dd91e96435a28d082f7581dde2b3da46857a97a
Hae-Riri <ehl3288@naver.com>
2023-09-06 21:57:30 +0900

    23.09.06 1번 다단계칫솔판매

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,30 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class Main {
+    private Map<String, String> parents;
+
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
-        int answer = solution.solution(maps);
-        System.out.println(answer);
-    }
-}
-
-class Solution {
-    public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] visit = new boolean[n][m];
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        Queue<Point> points = new LinkedList<>();
-
-        visit[0][0] = true;
-        points.add(new Point(0, 0, 1));
-
-        while(!points.isEmpty()) {
-            Point poll = points.poll();
-            if(poll.x == n -1 && poll.y == m-1) {
-                return poll.dist;
-            }
-            for(int i = 0; i < 4; i++) {
-                int x = poll.x + dx[i];
-                int y = poll.y + dy[i];
-                if(x < 0 || x >= n || y < 0 || y >= m) {
-                    continue;
-                }
-                if(!visit[x][y] && maps[x][y] == 1) {
-                    visit[x][y] = true;
-                    points.add(new Point(x, y, poll.dist + 1));
-                }
-            }
-        }
-
-        return -1;
+        Main main = new Main();
+        main.init();
+        main.getParent("hi");
     }
 
-    class Point {
-        int x;
-        int y;
-        int dist;
-        Point(int x, int y, int dist) {
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
+    private void init() {
+        parents = new HashMap<>();
+        parents.put("john", "-");
+        parents.put("mary", "-");
+        parents.put("edward", "john");
+        parents.put("hi", "edward");
+    }
+
+    private void getParent(String child) {
+        if("-".equals(child)) {
+            return;
         }
+        System.out.println(child);
+        getParent(parents.get(child));
     }
 }
---
+++ src/main/java/programmers/PG_다단계칫솔판매.java
@@ -0,0 +1,80 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PG_다단계칫솔판매 {
+    public static void main(String[] args) {
+        String [] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
+        String [] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
+        String [] seller = {"sam", "emily", "jaimie", "edward"};
+        int[] amount = {2, 3, 5, 4};
+        Solution solution = new Solution();
+        int[] solution1 = solution.solution(enroll, referral, seller, amount);
+        for(int i = 0; i < solution1.length; i++) {
+            System.out.print(solution1[i] + " ");
+        }
+    }
+}
+
+class Solution {
+
+    private Map<String, String> parents;// child -> Parent
+    private Map<String, Integer> results; // -는 민호.
+
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        init(enroll, referral);
+        calculate(seller, amount);
+
+        int[] answer = new int[enroll.length];
+        for(int i = 0; i < enroll.length; i++) {
+            answer[i] = results.get(enroll[i]);
+        }
+        return answer;
+    }
+
+    private void init(String[] enroll, String[] referral) {
+        parents = new HashMap<>();
+        results = new HashMap<>();
+        results.put("-", 0);
+
+        for(int i = 0; i < enroll.length; i++) {
+            String child = enroll[i];
+            results.put(child, 0);
+
+            String parent = referral[i];
+            parents.put(child, parent);
+        }
+    }
+
+    private void calculate(String[] seller, int[] amount) {
+        for(int i = 0; i < seller.length; i++) {
+            String sellerName = seller[i];
+            double sellPrice = amount[i] * 100;
+
+            getParent(sellerName, sellPrice);
+
+            // 디버깅용 출력
+            System.out.println("---");
+            for(String key : results.keySet()) {
+                System.out.println(key + ": " + results.get(key));
+            }
+        }
+    }
+
+    private void getParent(String child, double price) {
+        if("-".equals(child)) {
+            results.put("-", results.getOrDefault("-", 0) + (int)price);
+            return;
+        }
+
+        double calculated = price * 0.1;
+        if(calculated >= 1) {
+            results.put(child, results.getOrDefault(child, 0) + (int)(price) - (int)calculated); // mary: 11
+            getParent(parents.get(child), calculated); // -, 1
+        } else {
+            results.put(child, results.getOrDefault(child, 0) + (int)price);
+            return;
+        }
+    }
+}
