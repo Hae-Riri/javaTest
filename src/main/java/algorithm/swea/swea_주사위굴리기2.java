c6717484fcd4faa211c99b1c155cabc6753e675b
Hae-Riri <ehl3288@naver.com>
2023-09-12 21:23:26 +0900

    23.09.12

--- src/main/java/algorithm/swea/swea_주사위굴리기2.java
+++ src/main/java/algorithm/swea/swea_주사위굴리기2.java
@@ -11,12 +11,11 @@ public class swea_주사위굴리기2 {
     static int n, m, k;
     static int map[][];
     static boolean visit[][];
-
     // 북 동 남 서
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int d = 1;
-    static int[] dice = {2,6,5,1,4,3}; //0 1 2 3 4 5
+    static int[] dice = {2,1,5,6,4,3}; //0 1 2 3 4 5
     static int score = 0;
     static XY xy = new XY(0,0);
 
@@ -65,33 +64,34 @@ public class swea_주사위굴리기2 {
                 for(int i = 0; i < 4; i++) {
                     int nextX = poll.x + dx[i];
                     int nextY = poll.y + dy[i];
-                    if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m || visit[nextX][nextY]) {
+                    if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) {
                         continue;
                     }
-                    if(map[nextX][nextY] == map[xy.x][xy.y]) {
+                    if(!visit[nextX][nextY] && map[nextX][nextY] == map[xy.x][xy.y]) {
                         q.add(new XY(nextX, nextY));
                         visit[nextX][nextY] = true;
                     }
                 }
             }
 
-            score += (map[xy.x][xy.y] * cnt);
+            score += map[xy.x][xy.y] * cnt;
 
             // 주사위를 굴린 다음 A:map, B:dice[1]을 비교해서 d 정하기.
             rotateDice(d);
             int B = map[xy.x][xy.y];
-            int A = dice[1];
+            int A = dice[3];
+//            int A = dice2[1][3];
             if(A > B) {
                 d = (d + 1) % 4;
             } else if(A < B) {
-//                d = d -1;
-//                if(d < 0) {
-//                    d = 3;
-//                }
-                d = (d + 3) % 4;
+                d = d -1;
+                if(d < 0) {
+                    d = 3;
+                }
             }
             // 암것도 아니면 d는 그대로임.
         }
+
         System.out.println(score);
     }
 
@@ -112,13 +112,14 @@ public class swea_주사위굴리기2 {
 
     public static void rotateDice(int d) {
         if (d == 0) { //북
-            dice = new int[]{dice[2], dice[0], dice[1], dice[3], dice[4], dice[5]};
-        } else if (d == 1) { // 동
-            dice = new int[]{dice[0], dice[5], dice[2], dice[4], dice[1], dice[3]};
-        } else if (d == 2) { // 남
             dice = new int[]{dice[1], dice[2], dice[3], dice[0], dice[4], dice[5]};
-        } else { // 서
+        } else if (d == 1) { // 동
             dice = new int[]{dice[0], dice[4], dice[2], dice[5], dice[3], dice[1]};
+        } else if (d == 2) { // 남
+            dice = new int[]{dice[3], dice[0], dice[1], dice[2], dice[4], dice[5]};
+        } else { // 서
+            dice = new int[]{dice[0], dice[5], dice[2], dice[4], dice[1], dice[3]};
         }
+
     }
 }
