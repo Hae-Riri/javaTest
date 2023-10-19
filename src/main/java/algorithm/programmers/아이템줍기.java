23f9255413362e77ae9df31540dab40638f219f3
Hae-Riri <ehl3288@naver.com>
2023-10-19 09:36:40 +0900

    23.10.18 아이템줍기 다시 품
    - rectangle에 대한 for문을 두번을 돌려야 내부에 겹치는 영역들이 다같이 0이 된다. for문 한번 안에서 외부, 내부를 돌리면

--- src/main/java/algorithm/programmers/아이템줍기.java
+++ src/main/java/algorithm/programmers/아이템줍기.java
@@ -10,74 +10,71 @@ public class 아이템줍기 {
         System.out.println(solution);
     }
 
-
-
     public static int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int[] dx = {-1, 1, 0 ,0};
+        int[][] map = new int[101][101]; // 1~100
+        int answer = Integer.MAX_VALUE;
+        initMap(map, rectangle);
+
+        characterX *= 2;
+        characterY *= 2;
+        itemX *= 2;
+        itemY *= 2;
+
+        int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        int minDist = Integer.MAX_VALUE;
-        int[][] map = new int[101][101];
+
+        Queue<XY> q = new LinkedList<>();
         boolean[][] visit = new boolean[101][101];
 
-        for(int[] data : rectangle) {
-            // 0, 2번째가 x값, 1,3번째가 y값
-            // 꼭짓점 테두리를 포함해서 내부까지 전부 1로 만듦
-            for(int i = data[0] * 2; i <= data[2] * 2; i++) {
-                for(int j = data[1] * 2; j <= data[3] * 2; j++) {
+        q.add(new XY(characterX, characterY, 0));
+        visit[characterX][characterY] = true;
+
+        while(!q.isEmpty()) {
+            XY now = q.poll();
+            if(now.x == itemX && now.y == itemY) {
+                answer = Integer.min(answer, now.dist / 2);
+            }
+            for(int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+                if(nx < 0 || nx >= 101 || ny < 0 || ny >= 101 || visit[nx][ny] || map[nx][ny] == 0) {
+                    continue;
+                }
+                q.add(new XY(nx, ny, now.dist+1));
+                visit[nx][ny] = true;
+            }
+        }
+        return answer;
+    }
+
+    static class XY {
+        int x;
+        int y;
+        int dist;
+        public XY(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+
+    public static void initMap(int[][] map, int[][] rectangle) {
+        for(int[] points: rectangle) {
+            // 전체를 1로 만들고
+            for(int i = points[0] * 2; i <= points[2] * 2; i++) {
+                for(int j = points[1] * 2; j <= points[3] * 2; j++) {
                     map[i][j] = 1;
                 }
             }
         }
 
-        for(int[] data : rectangle) {
-            // 테두리 제외하고는 전부 0으로 바꿈. (이러면 테두리만 1로 남는다)
-            for(int i = data[0] * 2 + 1; i < data[2] * 2; i++) {
-                for(int j = data[1] * 2 + 1; j < data[3] * 2; j++) {
+        for(int[] points : rectangle) {
+            // 내부를 0으로 만들어서 테두리만 1로 남기기
+            for(int i = points[0] * 2 + 1; i < points[2] * 2 ; i++) {
+                for(int j = points[1] * 2 + 1; j < points[3] * 2; j++) {
                     map[i][j] = 0;
                 }
             }
-        }
-
-        for(int i = 0; i < 15; i++) {
-            for(int j = 0; j < 15; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(characterX * 2, characterY * 2, 0));
-        visit[characterX * 2][characterY * 2] = true;
-
-        while(!q.isEmpty()) {
-            Point poll = q.poll();
-            if(poll.x == itemX * 2 && poll.y == itemY * 2) {
-                minDist = Integer.min(poll.dist, minDist);
-            }
-
-            for(int d = 0; d < 4; d++) {
-                int nx = poll.x + dx[d];
-                int ny = poll.y + dy[d];
-                if(nx < 0 || nx >= 101 || ny < 0 || ny >= 101 || map[nx][ny] == 0 || visit[nx][ny]) {
-                    continue;
-                }
-
-                q.add(new Point(nx, ny, poll.dist + 1));
-                visit[nx][ny] = true;
-            }
-        }
-
-        return minDist / 2;
-    }
-
-    static class Point {
-        int x;
-        int y;
-        int dist;
-        Point(int x, int y, int dist) {
-            this.x = x;
-            this.y = y;
-            this.dist= dist;
         }
     }
 }
